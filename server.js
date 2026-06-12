b7b822cfbdfff0208a812d6d5514c5ce5c8bf26b
bhernaurd <bhernaurdm@gmail.com>
2026-06-12 18:40:47 -0500

    Update server.js

--- server.js
+++ server.js
@@ -108,7 +108,7 @@ const managedMessages = await manageHistory(validMessages);
 
 ```
 const response = await client.messages.create({
-  model: 'claude-haiku-4-5-20250929',
+  model: 'claude-haiku-4-5-20251001',
   max_tokens: 1024,
   system,
   messages: managedMessages
