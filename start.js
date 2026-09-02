1af78d7aa462745ebbd68993fc4906bed3a33c8f
bhernaurd <bhernaurdm@gmail.com>
2026-09-02 15:05:29 -0500

    Start Apple subscription status reconciliation worker

--- start.js
+++ start.js
@@ -1,4 +1,5 @@
 // Production entrypoint. Load environment first, then background workers, then HTTP server.
 import './env.js';
 import './appleProceedsSyncWorker.js';
+import './appleSubscriptionStatusReconciliationWorker.js';
 import './server.js';
